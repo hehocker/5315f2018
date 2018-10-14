4c6331eff047da1f24133b5a67fcd1d20ccd55e1
haley <hockerh@seattleu.edu>
2018-10-14 10:18:27 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,7 +1,7 @@
 {
   "data": [
     {
-      "first_name": "James Bond",
+      "first_name": "James",
       "designation": "Accountant",
       "salary": "$170,750",
       "joining_date": "2011/07/25",
